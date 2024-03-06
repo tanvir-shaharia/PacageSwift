737178fa7cff0d299970fd0e8dfab1b408e62d18
MD Tanvir Shaharia <tanvirking29@gmail.com>
2024-03-06 15:59:01 +0600

    added

--- Sources/MyFirstPackage/MainView.swift
+++ Sources/MyFirstPackage/MainView.swift
@@ -10,8 +10,16 @@ import SwiftUI
 @available(iOS 16.0, *)
 public struct MainView: View {
  public init() {} 
-   public var body: some View {
-       Text("Package is now working")
+    @available(macOS 10.15, *)
+    public var body: some View {
+        Text("Package is now working")
+            .padding()
+            .font(.title)
+            .foregroundColor(.blue)
+        Text("Package is now working")
+            .padding()
+            .font(.title)
+            .foregroundColor(.blue)
     }
 }
 
